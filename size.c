5a0a64e74e2c5a007117b4aba6574a7d484dd572
jasonluyk <jasonluyk1@gmail.com>
2026-07-02 09:22:43 -0400

    C Projects

---
+++ size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("char: %lu\n", sizeof(char));
+    printf("short: %lu\n", sizeof(short));
+    printf("int: %lu\n", sizeof(int));
+    printf("long: %lu\n", sizeof(long));
+    printf("long long: %lu\n", sizeof(long long));
+    printf("float: %lu\n", sizeof(float));
+    printf("double: %lu\n", sizeof(double));
+    printf("long double: %lu\n", sizeof(long double));
+}
